20f8668fd37b898f35c4fca920d9b187abd37cc3
rdfriedl <rdfriedl@gmail.com>
2015-06-06 09:38:31 -0700

    added libraries
    added json files for games/libraries

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 function loadImage(url,cb){
 	$.ajax({
-		url: 'img/'+url,
+		url: url,
 		type: 'GET',
 		dataType: 'text'
 	})
@@ -9,14 +9,17 @@ function loadImage(url,cb){
 	})
 }
 
-games = [];
 $(document).ready(function() {
+	//game
 	$gameTemp = $('#temp .game');
 	$gameTemp.remove();
 	$gameImage = $('#temp .game-image');
 	$gameImage.remove();
 	$gameItem = $('#temp .item');
 	$gameItem.remove();
+	//library
+	$libraryTemp = $('#temp .library');
+	$libraryTemp.remove();
 	
 	$('#back-to-top').click(function(){
 		$.smoothScroll({offset: 0});
@@ -31,45 +34,56 @@ $(document).ready(function() {
 		}
 	});
 
-	//load json
+	//load games
 	$.ajax({
-		url: 'https://api.github.com/users/rdfriedl/repos',
+		url: 'data/games.json',
 		type: 'GET',
 		dataType: 'json'
 	})
 	.done(function(json) {
-		games = json;
 		//build game list
 		for (var i = 0; i < json.length; i++) {
 			var $game = $gameTemp.clone(true,true);
-			$game.find('.title').text(json[i].name);
-			$game.find('.desc').text(json[i].description);
-			$game.find('.source-link').attr('href',json[i].svn_url);
-			$game.find('.play-link').attr('href',json[i].homepage);
+			var game = json[i];
+			$game.find('.title').text(game.title);
+			$game.find('.desc').text(game.description);
+			$game.find('.source-link').attr('href',game.sourceURL);
+			$game.find('.play-link').attr('href',game.playURL);
 
-			//see if we can find its image
-			loadImage('games/'+json[i].name+'.png',function(game,$game) {
+			$game.find('.cover').attr('href',game.images[0]);
+			$game.find('.cover>img').attr('src',game.images[0]);
+			$game.appendTo('#games');
 
-				$game.find('.cover').attr('href','img/games/'+game.name+'.png');
-				$game.find('.cover>img').attr('src','img/games/'+game.name+'.png');
-				$game.appendTo('#games');
+			for (var k = 0; k < game.images.length; k++) {
+				loadImage(game.images[k],function(url,$game){
+					var $image = $gameImage.clone(true);
+					$image.find('a').attr('href',url)
+					$image.find('img').attr('src',url)
+					//add it
+					$game.find('.images').append($image);
+				}.bind(this,game.images[k],$game));
+			};
+		};
+	})
 
-				//find as many images as we can
-				var func = function(game,$game,imageNum){
-					loadImage('games/'+game.name+imageNum+'.png',function(imageNum,game,$game){
-						var $image = $gameImage.clone(true);
-						$image.find('a').attr('href','img/games/'+game.name+imageNum+'.png')
-						$image.find('img').attr('src','img/games/'+game.name+imageNum+'.png')
-						//add it
-						$game.find('.images').append($image);
-						imageNum++;
+	//load libraries
+	$.ajax({
+		url: 'data/libraries.json',
+		type: 'GET',
+		dataType: 'json'
+	})
+	.done(function(json) {
+		//build library list
+		for (var i = 0; i < json.length; i++) {
+			var $library = $libraryTemp.clone(true,true);
+			var library = json[i];
+			$library.find('.title').text(library.title);
+			$library.find('.desc').text(library.description);
+			$library.find('.source-link').attr('href',library.sourceURL);
+			$library.find('.site-link').attr('href',library.siteURL);
+			$library.find('.demo-link').attr('href',library.demoURL);
 
-						func(imageNum++);
-					}.bind(this,imageNum,game,$game))
-				}.bind(this,game,$game)
-
-				func(2);
-			}.bind(this,json[i],$game))
+			$library.appendTo('#libraries');
 		};
 	})
 
